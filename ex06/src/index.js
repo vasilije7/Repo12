50da760212fc488fa753ba8f89aefff1d5a3ce0f
Vasilije Vujovic <vasilijev7@gmail.com>
2022-04-08 14:37:10 +0200

    Vjezba 6

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+function myNes() {
+  var myStorage = {
+    car: {
+      inside: {
+        "glove box": "maps",
+        "passenger seat": "crumbs",
+      },
+      outside: {
+        trunk: "jack",
+      },
+    },
+  };
+  // Only change code below this line
+  var gloveBoxContents = myStorage.car.inside["glove box"];
+  // Only change code above this line
+  return gloveBoxContents;
+}
+console.log(myNes());
+module.exports = myNes;
